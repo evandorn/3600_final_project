451aac2fd16920bac9db1413bbd9bfcbe39a05c7
evan.dorn@gmail.com <evan.dorn@gmail.com>
2015-10-31 21:53:00 -0400

    Basic client code, src and test file structure.

---
+++ makefile
@@ -0,0 +1,21 @@
+# Makefile for CPSC 3600 Final Project
+
+CC=clang
+CFLAGS=-Wall -Wextra
+SRC=client.c server.c
+OBJ=$(addprefix src/, $(SRC:.c=.o))
+
+all: libmyalloc.so
+
+
+clean:
+	rm -f src/*.o
+	rm -f tests/mytestprogram
+	rm -f tests/main_test
+
+test:	all
+	$(CC) $(CFLAGS) tests/mytestprogram.c -o tests/mytestprogram
+	cd tests && ./mytestprogram
+test2:
+	$(CC) $(CFLAGS) tests/main_test.c -o tests/main_test
+	cd tests && LD_PRELOAD=../libmyalloc.so ./main_test
--- src/server.c
+++ src/server.c
@@ -5,9 +5,32 @@
 //  Created by Evan Dorn on 10/31/15.
 //  Copyright (c) 2015 Evan Dorn. All rights reserved.
 //
+// ===============Server Address Info========================
+//    HOST:
+//    PORT:
+// ==========================================================
+
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <netdb.h>
 
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+
+#define BUFFERSIZE 256                             // basic 256 CHAR buffer
+
+
+/***********************************************
+ *                  Main                       *
+ ***********************************************/
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
