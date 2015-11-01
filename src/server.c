ad54405f19f1121b2cd0670a1daf679513553e20
evan.dorn@gmail.com <evan.dorn@gmail.com>
2015-10-31 21:04:43 -0400

    Added initial source skeleton files

---
+++ src/server.c
@@ -0,0 +1,15 @@
+//
+//  server.c
+//  cpsc_3600_final_project
+//
+//  Created by Evan Dorn on 10/31/15.
+//  Copyright (c) 2015 Evan Dorn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
