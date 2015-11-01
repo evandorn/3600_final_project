e805af1034c576fef0410c5c15cfad8c70d64433
evan.dorn@gmail.com <evan.dorn@gmail.com>
2015-10-31 22:02:50 -0400

    added test skeleton

---
+++ tests/mytestprogram.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../src/"
+
+int main(void) {
+    printf("=========================\n");
+    printf("       TEST SUITE\n");
+    printf("=========================\n\n");
+    
+}
